03183c6dbd1bef1ae9da91fcbd08b936326db72e
Quân Nguyễn <quanlh3052004@gmail.com>
2026-06-18 22:30:50 +0700

    init project

---
+++ src/main/java/kuan/fintech/fintech_api_gateway/FintechApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package kuan.fintech.fintech_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kuan/fintech/fintech_api_gateway/FintechApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kuan.fintech.fintech_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
